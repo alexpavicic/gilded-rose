71f7777273aa3dcec73875db61a48f4e30655b10
Alex Pavicic <alexpavicicic@gmail.com>
2024-03-14 23:26:46 -0500

    Added Module, removed unnecessary methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,11 @@
-class GildedRose
+module GildedRose
   attr_reader :item
 
-  def initialize(name:, days_remaining:, quality:)
-    @item = klass_for(name).new(quality, days_remaining)
+  def self.new(name:, days_remaining:, quality:)
+    klass_for(name).new(quality, days_remaining)
   end
 
-  def klass_for (name)
+  def self.klass_for (name)
     case name
     when 'Normal Item'
       Normal
@@ -17,18 +17,6 @@ class GildedRose
       Backstage
     end
   end
-
-  def tick
-    item.tick
-  end
-
-  def quality
-    item.quality
-  end  
-
-  def days_remaining
-    @item.days_remaining
-  end  
 
   class Normal
     attr_reader :quality, :days_remaining
